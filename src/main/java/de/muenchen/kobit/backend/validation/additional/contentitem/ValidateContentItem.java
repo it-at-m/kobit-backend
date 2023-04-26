09557c9f4d4896a2721458ffdbaa1cc044229b50
Thomas Fink <thomas.fink@muenchen.de>
2023-04-26 12:25:45 +0200

    spotless

--- src/main/java/de/muenchen/kobit/backend/validation/additional/contentitem/ValidateContentItem.java
+++ src/main/java/de/muenchen/kobit/backend/validation/additional/contentitem/ValidateContentItem.java
@@ -19,19 +19,15 @@ public class ValidateContentItem implements ContentItemValidator<ContentItemView
         if (contentItemView.getContent() == null || contentItemView.getContent().trim().isEmpty()) {
             throw new InvalidContentItemException("Content string cannot be blank or null.");
         }
-
         if (contentItemView.getPageType() == null) {
             throw new InvalidContentItemException("PageType cannot be null.");
         }
-
         if (isContentOutOfRange(contentItemView.getContent())) {
             throw new InvalidContentItemException(
                     "Header must be at least 5 characters and not more than 5000!");
         }
-        // Add more validation rules here if needed
-    }
 
-    // Add helper methods for additional validation rules if needed
+    }
 
     private boolean isContentOutOfRange(String header) {
         return header.length() < CONTENT_MIN_SIZE || header.length() > CONTENT_MAX_SIZE;
--- src/main/java/de/muenchen/kobit/backend/validation/additional/textitem/ValidateTextItem.java
+++ src/main/java/de/muenchen/kobit/backend/validation/additional/textitem/ValidateTextItem.java
@@ -18,36 +18,26 @@ public class ValidateTextItem implements TextItemValidator<TextItemView> {
         if (textItemView == null) {
             throw new InvalidTextItemException("TextItemView can not be null!");
         }
-        // Check if header is null or empty
         if (textItemView.getHeader() == null || textItemView.getHeader().trim().isEmpty()) {
             throw new InvalidTextItemException("Header cannot be null or an empty string.");
         }
-
-        // Check if entry is null or empty
         if (textItemView.getEntry() == null || textItemView.getEntry().trim().isEmpty()) {
             throw new InvalidTextItemException("Entry cannot be null or an empty string.");
         }
-
-        // Check if pageType is null
         if (textItemView.getPageType() == null) {
             throw new InvalidTextItemException("PageType cannot be null.");
         }
-
         if (isHeaderOutOfRange(textItemView.getHeader())) {
             throw new InvalidTextItemException(
                     "Header must be at least 5 characters and not more than 100!");
         }
-
         if (isEntryCutOutOfRange(textItemView.getEntry())) {
             throw new InvalidTextItemException(
                     "Entry must be at least 5 characters and not more than 500!");
         }
 
-        // Add more validation rules here if needed
 
     }
-
-    // Add helper methods for additional validation rules if needed
 
     private boolean isHeaderOutOfRange(String header) {
         return header.length() < HEADER_MIN_SIZE || header.length() > HEADER_MAX_SIZE;
