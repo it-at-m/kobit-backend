5cac0536a2ea019109a9e6e7af4e329e8e0b3014
clemens-code <clemens.schuetze@outlook.de>
2023-04-28 14:21:46 +0200

    add schema for custom queries

--- src/main/java/de/muenchen/kobit/backend/contactpoint/repository/ContactPointRepository.java
+++ src/main/java/de/muenchen/kobit/backend/contactpoint/repository/ContactPointRepository.java
@@ -4,27 +4,29 @@ import de.muenchen.kobit.backend.contactpoint.model.ContactPoint;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import javax.persistence.NamedNativeQuery;
+
 
 public interface ContactPointRepository extends JpaRepository<ContactPoint, UUID> {
 
     Optional<ContactPoint> findContactPointByShortCut(String shortCut);
 
-    @Query(
-            nativeQuery = true,
-            value =
-                    "SELECT p.id, p.name, p.short_cut, p.description, d.department FROM"
-                        + " contact_point as p LEFT JOIN departments as d ON p.id ="
+    @Query(value = "SELECT p.id, p.name, p.short_cut, p.description, d.department FROM "
+                        + " {h-schema}contact_point as p LEFT JOIN {h-schema}departments as d ON p.id ="
                         + " d.contact_point_id WHERE d.department LIKE :department OR d.department"
-                        + " IS NULL")
+                        + " IS NULL",
+            nativeQuery = true)
     List<ContactPoint> findAllByDepartmentLikeOrNull(@Param("department") String department);
 
     @Query(
             nativeQuery = true,
             value =
-                    "SELECT * FROM contact_point as p LEFT JOIN departments d on p.id ="
+                    "SELECT * FROM {h-schema}contact_point as p LEFT JOIN {h-schema}departments d on p.id ="
                         + " d.contact_point_id WHERE p.id = :id AND (d.department LIKE :department"
                         + " OR d.department IS NULL)")
     Optional<ContactPoint> findContactPointByIdAndDepartmentLike(
