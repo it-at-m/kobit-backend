a0a6dfcc8d6dd9f7d3de8926e4be79986f5f33f6
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2023-09-14 14:47:09 +0200

    Update UserDataResolver.java

--- src/main/java/de/muenchen/kobit/backend/user/service/UserDataResolver.java
+++ src/main/java/de/muenchen/kobit/backend/user/service/UserDataResolver.java
@@ -42,11 +42,9 @@ public class UserDataResolver {
     private User readUserFromToken(Authentication authentication) {
         if (authentication instanceof JwtAuthenticationToken) {
             log.info("Authentication instance of JwtAuthenticationToken");
-            final JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
-            
+            final JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;      
             // Logging the length of the token here
             log.info("Token length: {}", jwtToken.getToken().getTokenValue().length());
-            
             Map<String, Object> tokenAttributes = jwtToken.getTokenAttributes();
             try {
                 return new User(
