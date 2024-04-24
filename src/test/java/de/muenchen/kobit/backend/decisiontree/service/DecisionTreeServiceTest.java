50c7ab451be9ae0605fbf37f268de0614c114e9d
Thomas Fink <thomas.fink@muenchen.de>
2024-04-24 11:14:45 +0200

    run spotless apply

--- src/test/java/de/muenchen/kobit/backend/decisiontree/service/DecisionTreeServiceTest.java
+++ src/test/java/de/muenchen/kobit/backend/decisiontree/service/DecisionTreeServiceTest.java
@@ -1,20 +1,14 @@
 package de.muenchen.kobit.backend.decisiontree.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import de.muenchen.kobit.backend.competence.Competence;
 import de.muenchen.kobit.backend.competence.service.CompetenceService;
-import de.muenchen.kobit.backend.contactpoint.view.ContactPointView;
 import de.muenchen.kobit.backend.decisiontree.branches.*;
 import de.muenchen.kobit.backend.decisiontree.relevance.service.RelevanceService;
 import de.muenchen.kobit.backend.decisiontree.view.DecisionPoint;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
