f79bedf2564e070e27bb9fa3b1e65763b4652dde
Thomas Fink <thomas.fink@muenchen.de>
2024-04-24 14:19:50 +0200

    spotless apply

--- src/test/java/de/muenchen/kobit/backend/validation/ValidateContactPointTest.java
+++ src/test/java/de/muenchen/kobit/backend/validation/ValidateContactPointTest.java
@@ -227,7 +227,8 @@ class ValidateContactPointTest {
                 assertThrows(
                         InvalidContactPointException.class,
                         () -> validateContactPoint.validate(contactPointView));
-        assertThat(exception.getMessage()).isEqualTo("Name must be at least 4 characters and not more than 100!");
+        assertThat(exception.getMessage())
+                .isEqualTo("Name must be at least 4 characters and not more than 100!");
     }
 
     @Test
