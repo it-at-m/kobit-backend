5dcc6bc11b576d5debe5392986b8d349377c7408
jannik.lange <jannik.lange@muenchen.de>
2024-11-13 10:36:11 +0100

    :art: spotless

--- src/main/java/de/muenchen/kobit/backend/contactpoint/service/ContactPointManipulationService.java
+++ src/main/java/de/muenchen/kobit/backend/contactpoint/service/ContactPointManipulationService.java
@@ -100,10 +100,11 @@ public class ContactPointManipulationService {
                                 it.getCompetences())); // set new competences for all
     }
 
-    private void removeUnusedCompetences(ContactPointListItem contactPointListItem, List<Competence> competences) {
+    private void removeUnusedCompetences(
+            ContactPointListItem contactPointListItem, List<Competence> competences) {
         log.debug("removeUnusedCompetences | competences: {}", competences.toString());
         Path currentPath = relevanceService.getPath(new HashSet<>(competences));
-        if (currentPath ==null) {
+        if (currentPath == null) {
             return;
         }
         log.debug("removeUnusedCompetences | currentPath: {}", currentPath.toString());
