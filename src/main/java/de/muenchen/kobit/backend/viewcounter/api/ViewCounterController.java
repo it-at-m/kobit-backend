f03f4d6eed5dd2216c2091d72d420ac6de89e31a
langehm <38280183+langehm@users.noreply.github.com>
2025-08-06 16:32:14 +0200

    :alien: added controller with enpoints

---
+++ src/main/java/de/muenchen/kobit/backend/viewcounter/api/ViewCounterController.java
@@ -0,0 +1,35 @@
+package de.muenchen.kobit.backend.viewcounter.api;
+
+import de.muenchen.kobit.backend.admin.service.AdminService;
+import de.muenchen.kobit.backend.viewcounter.model.ViewCounterCategory;
+import de.muenchen.kobit.backend.viewcounter.service.ViewCounterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/viewcounter")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ViewCounterController {
+
+    private final AdminService adminService;
+    private final ViewCounterService viewCounterService;
+
+    @GetMapping("/{category}")
+    @PreAuthorize("@adminService.isUserKobitAdmin()")
+    public Long viewCounter(@PathVariable ViewCounterCategory category) {
+        return viewCounterService.getCurrentViewCounterValue(category);
+
+    }
+
+    @GetMapping("/sum/{category}")
+    @PreAuthorize("@adminService.isUserKobitAdmin()")
+    public Long viewCounterSummarized(@PathVariable ViewCounterCategory category) {
+        return viewCounterService.getViewCountsByCategory(category);
+
+    }
+}
