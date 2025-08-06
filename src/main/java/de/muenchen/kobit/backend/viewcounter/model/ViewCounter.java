e29a21c8398e3ee3d9a042cd233032871638f9ef
langehm <38280183+langehm@users.noreply.github.com>
2025-08-06 16:21:50 +0200

    :sparkles: added viewcounter entities

---
+++ src/main/java/de/muenchen/kobit/backend/viewcounter/model/ViewCounter.java
@@ -0,0 +1,56 @@
+package de.muenchen.kobit.backend.viewcounter.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity
+public class ViewCounter {
+
+    @Id
+    @NotNull
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @NotNull
+    @Column(unique = true, nullable = false)
+    private ViewCounterCategory category;
+
+    @Column(nullable = false)
+    private Long value = 0L;
+
+    @Column()
+    @Setter(AccessLevel.NONE)
+    private LocalDate deactivatedAt;
+
+    @PrePersist
+    void prePersist() {
+        if (this.value == null){
+            this.value = 0L;
+        }
+    }
+
+    public ViewCounter deactivate() {
+        this.deactivatedAt = LocalDate.now();
+        return this;
+    }
+
+    public boolean isActive() {
+        return this.deactivatedAt == null;
+    }
+
+    public void incrementCounter() {
+        if (this.deactivatedAt == null){
+            this.value++;
+        }
+    }
+
+}
---
+++ src/main/java/de/muenchen/kobit/backend/viewcounter/model/ViewCounterCategory.java
@@ -0,0 +1,6 @@
+package de.muenchen.kobit.backend.viewcounter.model;
+
+public enum ViewCounterCategory {
+    E_MAIL_SEND_COUNTER,
+    PAGE_VISITED_COUNTER
+}
