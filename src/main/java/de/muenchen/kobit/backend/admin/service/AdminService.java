0cd1f86586a856b5754b6dfc1bb972a146454b06
langehm <38280183+langehm@users.noreply.github.com>
2025-08-19 13:22:25 +0200

    added view counter to admin endpoint

--- src/main/java/de/muenchen/kobit/backend/admin/service/AdminService.java
+++ src/main/java/de/muenchen/kobit/backend/admin/service/AdminService.java
@@ -3,25 +3,32 @@ package de.muenchen.kobit.backend.admin.service;
 import de.muenchen.kobit.backend.admin.model.AdminUserView;
 import de.muenchen.kobit.backend.user.service.UserDataResolver;
 import java.util.List;
+
+import de.muenchen.kobit.backend.viewcounter.model.ViewCounterCategory;
+import de.muenchen.kobit.backend.viewcounter.service.ViewCounterService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AdminService {
 
     private static final String DEPARTMENT_ADMIN = "lhm-ab-kobit-bereichsadmin";
 
     private static final String KOBIT_ADMIN = "lhm-ab-kobit-zentraleradmin";
 
+    private final ViewCounterService viewCounterService;
     private final UserDataResolver userDataResolver;
-
-    AdminService(UserDataResolver userDataResolver) {
-        this.userDataResolver = userDataResolver;
-    }
 
     public boolean isUserAdmin() {
         boolean isAdmin =
                 getUserRoles().stream()
                         .anyMatch(it -> it.equals(DEPARTMENT_ADMIN) || it.equals(KOBIT_ADMIN));
+
+        viewCounterService.incrementCounter(ViewCounterCategory.PAGE_VISITED_COUNTER);
+
         return isAdmin;
     }
 
